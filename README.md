f06e2d306c88975d67e2cf85d18c66c7a186d909
Dan Silvers <dan@silvers.net>
2023-11-22 23:36:37 -0600

    docker tweaks, sqlite setup

--- README.md
+++ README.md
@@ -11,4 +11,12 @@ On production, you probably want in `.env`:
 USER_RUN=1001
 GROUP_RUN=1001
 PORT=9001
+
+DJANGO_SETTINGS_MODULE=rioaeroclub.settings
+DJANGO_SECRET_KEY=''
+```
+
+```
+systemctl stop docker-compose@rioaeroclub
+systemctl start docker-compose@rioaeroclub
 ```
--- rioaeroclub/settings.py
+++ rioaeroclub/settings.py
@@ -113,13 +113,9 @@ WSGI_APPLICATION = 'rioaeroclub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '.db/db.sqlite3',
     }
 }
-
-#DATABASES = {
-#    "default": env.db("DATABASE_URL"),
-#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
